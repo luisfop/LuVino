baae2f2dc0e2b18e386d6d626ecc16a125bb9ca8
luisop <luisfop77@gmail.com>
2021-04-05 20:30:01 -0300

    fixing appbar to become responsive

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-import { AppBar, Typography , Grid } from "@material-ui/core";
+import { AppBar, Typography, Grid } from "@material-ui/core";
 
-import logo from '../../assets/images/Luvino-logo.png';
+import logo from "../../assets/images/Luvino-logo.png";
 
 import useStyles from "./style";
 
@@ -10,26 +10,29 @@ const Navbar = () => {
   const classes = useStyles();
 
   return (
-    <AppBar color="secondary" position="static" className={classes.navbar}>
-
+    <AppBar color="secondary" position="fixed" className={classes.navbar}>
       <div className={classes.logoBox}>
         <img src={logo} className={classes.logo} />
       </div>
-    <Grid container justify="space-around" className={classes.list}>
+      <Grid container justify="space-around" className={classes.list}>
+        <Grid item>
+          <Typography className={classes.links}>Home</Typography>
+        </Grid>
+        <Grid item>
+          <Typography className={classes.links}>Adega</Typography>
+        </Grid>
+        <Grid item>
+          <Typography className={classes.links}>Contato</Typography>
+        </Grid>
 
-      <Grid item >
-        <Typography className={classes.links}>Home</Typography>
-      </Grid >
-      <Grid item>
-        <Typography className={classes.links}>Xablau</Typography>
-      </Grid >
-      <Grid item>
-        <Typography className={classes.links}>Wines</Typography>
-      </Grid >
 
+        <div className={classes.mobile}>
+          <button >click me!</button>
+        </div>
+        
+      </Grid>
 
-    </Grid>
-
+      
     </AppBar>
   );
 };
--- src/components/navbar/style.js
+++ src/components/navbar/style.js
@@ -1,9 +1,12 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles(() => ({
+
+export default makeStyles((theme) => ({
     navbar:{
-        height: 70,
+        height: '70px',
         display: '-webkit-inline-box',
+        width: '100%',
+        
     },
     list: {
         backgroundColor: 'inherit',
@@ -13,23 +16,31 @@ export default makeStyles(() => ({
     },
     links: {
         backgroundColor: 'inherit',
-
-        '&:hover': {
-            background: "#f00",
-         },
+        fontWeight: 500,
+        '&:hover': {},
+        [theme.breakpoints.down('sm')]:{
+            display: 'none',
+        }
     },
     logo:{
         height: 50,
         width: 50,
     },
     logoBox:{
-        paddingLeft: 20,
+        marginLeft: 20,
         width: 80,
         height: 'inherit',
         display: 'flex',
         alignItems: 'center',
+    },
+    mobile:{
+        display: 'none',
+        [theme.breakpoints.down('sm')]:{
+            display: 'flex',
+            width: 'inherit',
+        },
+        
     }
 
     
-
 }));
